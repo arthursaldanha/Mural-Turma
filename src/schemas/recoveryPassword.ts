8b7fe49f5096509e32eb173b5b0bc1b9c20fbf4c
Arthur Saldanha <saldanhadev@gmail.com>
2022-02-20 00:51:52 -0300

    :sparkles: feat: changing error message if passwords do not match

--- src/schemas/recoveryPassword.ts
+++ src/schemas/recoveryPassword.ts
@@ -6,7 +6,7 @@ export const recoveryPasswordSchema = yup
     password: yup.string().required('Este campo é obrigatório'),
     passwordConfirmation: yup
       .string()
-      .oneOf([yup.ref('password'), null], 'As senhas não correspondem entre si')
+      .oneOf([yup.ref('password'), null], 'As senhas não coincidem')
       .required('Este campo é obrigatório')
   })
   .required()
