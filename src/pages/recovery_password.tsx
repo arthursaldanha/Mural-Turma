f62df80ee305de0aa821fbd97d7f3154318e3026
Arthur Saldanha <saldanhadev@gmail.com>
2022-02-20 01:49:13 -0300

    :sparkles: feat: checking if token is in cookies for possible redirect

--- src/pages/recovery_password.tsx
+++ src/pages/recovery_password.tsx
@@ -1,8 +1,27 @@
+import { GetServerSideProps } from 'next'
+import { parseCookies } from 'nookies'
 import React from 'react'
 import RecoveryPassword from 'views/recoveryPassword'
 
 const RecoveryPasswordPage = () => {
   return <RecoveryPassword />
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { ['muralturma-token']: token } = parseCookies(ctx)
+
+  if (token) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
 
 export default RecoveryPasswordPage
