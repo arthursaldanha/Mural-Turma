222f18c24fdbac1654ea4c9239340b26c0003feb
Arthur Saldanha <saldanhadev@gmail.com>
2022-02-20 01:52:21 -0300

    :sparkles: chore: changing the default page title and meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,14 +13,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Mural Turma</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Projeto criado para informar notícias sobre a turma"
         />
       </Head>
       <ThemeProvider theme={theme}>
