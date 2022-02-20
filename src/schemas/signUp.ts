445e96778e46a3c9e23d910c8f55ee28ac9e9f9f
Arthur Saldanha <saldanhadev@gmail.com>
2022-02-20 01:14:17 -0300

    :sparkles: chore: editing signUp requirements

--- src/schemas/signUp.ts
+++ src/schemas/signUp.ts
@@ -9,20 +9,23 @@ export const signUpSchema = yup
       .required('Este campo é obrigatório'),
     lastName: yup
       .string()
-      .min(3, 'O sobrenome deve possuir no mínimo 3 caracteres')
+      .min(2, 'O sobrenome deve possuir no mínimo 2 caracteres')
       .required('Este campo é obrigatório'),
     email: yup
       .string()
       .email('Digite um email válido')
-      .matches(/^(\S+@academico.ifs.edu.br)+$/gm, {
-        message:
-          'O email acadêmico deve seguir o formato: @academico.ifs.edu.br'
-      })
+      .matches(
+        /^([a-z]{2,20}\.[a-z]{2,20}[0-9]{3}@academico\.ifs\.edu\.br)+$/gm,
+        {
+          message:
+            'O email acadêmico deve seguir o formato: nome.sobrenome000@academico.ifs.edu.br'
+        }
+      )
       .required('Este campo é obrigatório'),
     password: yup.string().required('Este campo é obrigatório'),
     passwordConfirmation: yup
       .string()
-      .oneOf([yup.ref('password'), null], 'As senhas não correspondem entre si')
+      .oneOf([yup.ref('password'), null], 'As senhas não coincidem')
       .required('Este campo é obrigatório')
   })
   .required()
